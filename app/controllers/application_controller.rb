a7c916960059b2427b9d65c7283b1c405c554de3
Ausin Formica <austin.formica@gmail.com>
2015-02-01 15:35:05 -0500

    application controller singe quote

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
   def twitter_client
 
     Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["twitter_key"]
-      config.consumer_secret     = ENV["twitter_secret"]
-      config.access_token        = ENV["twitter_token"]
-      config.access_token_secret = ENV["twitter_token_secret"]
+      config.consumer_key        = ENV['twitter_key']
+      config.consumer_secret     = ENV['twitter_secret']
+      config.access_token        = ENV['twitter_token']
+      config.access_token_secret = ENV['twitter_token_secret']
     end
 
   end
